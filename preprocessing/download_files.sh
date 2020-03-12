6aff8ccd443441b2f9d4e5f4da77064420e4d09b
Junus Maharadze <junus.maharadze@gmail.com>
2020-03-12 16:52:26 +0000

    Added script to download the time series csv files

--- preprocessing/download_files.sh
+++ preprocessing/download_files.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 # Download time series files from github repo: https://github.com/CSSEGISandData/COVID-19
+
+mkdir ./../data
+wget -O ../data/time_series_19-covid-Confirmed.csv https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+wget -O ../data/time_series_19-covid-Deaths.csv https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
+wget -O ../data/time_series_19-covid-Recovered.csv https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv
