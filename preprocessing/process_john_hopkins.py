904c13f3d5dd5b202bbf91b4903761766ab51772
Junus Maharadze <junus.maharadze@gmail.com>
2020-04-08 13:36:11 +0100

    Implemented conversion of COVID-19 data to JSON
    Also changed the paths of downloaded data into folders separating EEA and JH data

---
+++ preprocessing/process_john_hopkins.py
@@ -0,0 +1,82 @@
+import json
+import pandas as pd
+import os
+from collections import defaultdict
+
+
+# TODO: add documentation
+
+
+def load_dataframes(data_dir, country_list, country_colname='Country/Region'):
+    # Set up paths for each csv file
+    confirmed_path = os.path.join(data_dir, 'time_series_covid19_confirmed_global.csv')
+    deaths_path = os.path.join(data_dir, 'time_series_covid19_deaths_global.csv')
+    recovered_path = os.path.join(data_dir, 'time_series_covid19_recovered_global.csv')
+
+    # Load the csv as dataframes and extract the relevant countries
+    df_confirmed = pd.read_csv(confirmed_path)
+    df_confirmed = df_confirmed.loc[df_confirmed[country_colname].isin(country_list)]
+
+    df_deaths = pd.read_csv(deaths_path)
+    df_deaths = df_deaths.loc[df_deaths[country_colname].isin(country_list)]
+
+    df_recovered = pd.read_csv(recovered_path)
+    df_recovered = df_recovered.loc[df_recovered[country_colname].isin(country_list)]
+
+    return df_confirmed, df_deaths, df_recovered
+
+
+def full_dataframe_for_day(df_confirmed, df_deaths, df_recovered, day):
+    # Combine data from each dataframe into one dictionary
+    d = {'Country/Region': df_confirmed['Country/Region'].values,
+         'confirmed': df_confirmed[day].values,
+         'deaths': df_deaths[day].values,
+         'recovered': df_recovered[day].values}
+
+    # Convert to dataframe
+    df = pd.DataFrame(data=d)
+
+    return df
+
+
+def day_dataframe_to_dict(df):
+    # Transpose dataframe to have the countries as column
+    df = df.transpose()
+
+    # Replace header with country names
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = new_header
+
+    # Convert resulting dataframe to dictionary
+    dictionary = df.to_dict()
+
+    return dictionary
+
+
+def main():
+    output_dict = defaultdict(dict)
+
+    data_dir = os.path.join(os.getcwd(), 'data', 'john_hopkins')
+    output_filename = "john_hopkins.json"
+    output_path = os.path.join(data_dir, output_filename)
+
+    # TODO: change country list to all countries in Europe, for now only testing with Italy and Spain
+    country_list = ['Italy', 'Spain']
+    df_confirmed, df_deaths, df_recovered = load_dataframes(data_dir, country_list)
+
+    # Extract list of days from header
+    days = df_confirmed.columns[4:]
+
+    # Loop over all days
+    for day in days:
+        day_df = full_dataframe_for_day(df_confirmed, df_deaths, df_recovered, day)
+        output_dict[day] = day_dataframe_to_dict(day_df)
+
+    # Write dictionary to JSON
+    with open(output_path, 'w') as fp:
+        json.dump(output_dict, fp, indent=4)
+
+
+if __name__ == '__main__':
+    main()
