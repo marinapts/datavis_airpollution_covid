26b621ba825656c617415556eeb8e4629a72c986
marinapts <potsimarina@gmail.com>
2020-04-12 23:38:31 +0100

    Change gradient of heatmap

--- visualisation/src/mapContainer/GoogleMapContainer.js
+++ visualisation/src/mapContainer/GoogleMapContainer.js
@@ -120,13 +120,33 @@ class GoogleMapContainer extends Component {
     }
     positions = this.processPositions(positions)
 
-    return {positions, options: { radius: 15, opacity: 0.6 }}
+    return {
+      positions,
+      options: { radius: 15, opacity: 0.6,
+        gradient: [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+        ]
+      }
+    }
   }
 
   render() {
     const { center, zoom } = this.state
     const heatMapData = this.getHeatmapData(this.props.airPollutionData)
-
+    console.log('process', process.env)
     return (
       <div style={{width: '100%', height: '800px'}}>
         <GoogleMapReact
